60fa95209544a850123c11117adb36e395bdd5c5
thibault livran <thibault.livran@dnd.fr>
2024-02-12 10:35:02 +0100

    AssertConfig: Add API resource annotations to entities

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -2,35 +2,53 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ActorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['actor:read'],'enable_max_depth'=>"true"]
+)]
 #[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'first_name' => 'partial', 'last_name' => 'partial', ])]
 class Actor
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['actor:read', 'movie:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 70)]
+    #[Groups(['movie:read', 'actor:read'])]
+    #[Assert\NotBlank(message: 'Le prénom est obligatoire')]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 70)]
+    #[Groups(['movie:read', 'actor:read'])]
+    #[Assert\NotBlank(message: 'Le nom est obligatoire')]
     private ?string $lastName = null;
 
     #[ORM\ManyToMany(targetEntity: Movie::class, mappedBy: 'actor')]
+    #[Groups(['actor:read'])]
+    #[MaxDepth(1)]
     private Collection $movies;
 
     #[ORM\ManyToOne(inversedBy: 'actor')]
+    #[Groups(['actor:read'])]
+    #[Assert\NotNull(message: 'La nationalité ne peut pas être nulle.')]
     private ?Nationality $nationality = null;
 
+    // Ajout des propriétés reward et nationality
     #[ORM\Column(nullable: true)]
+    #[Assert\Type(type: ['type' => 'string', 'message' => 'La récompense doit être une chaîne de caractères.'])]
     private ?string $reward = null;
 
     public function __construct()
